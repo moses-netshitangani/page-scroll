6f6aecd2d902daa9e1f9b5ad09b1719b21d51012
Moses Netshitangani <netshitanganimoses@gmail.com>
2022-12-06 21:10:58 +0200

    initial commit

---
+++ src/components/pages.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './pages.css'
+
+const Pages = () => {
+    return (
+        <div className="container">
+            <div>First Page</div>
+            <div>Second Page</div>
+            <div>Third Page</div>
+            <div>Fourth Page</div>
+        </div>
+    )
+}
+
+export default Pages;
